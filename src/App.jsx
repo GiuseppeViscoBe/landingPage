f9f9981f4bfc24fb72eb96e8fa3270f14e9075b5
Giuseppe Visco <g.visco.ita@be-tse.it>
2024-09-19 17:20:32 +0200

    development

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+
 import NavBar from "./components/navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import WorkFlowSection from "./components/WorkFlowSection";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <>
-       <NavBar />
+        <NavBar />
+        <div className="max-w-7xl mx-auto pt-20 px-6">
+          <HeroSection />
+          <FeatureSection />
+          <WorkFlowSection />
+          <Pricing />
+          <Testimonials />
+          <Footer />
+        </div>
       </>
     </div>
   );
